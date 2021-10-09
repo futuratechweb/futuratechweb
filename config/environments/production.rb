ffe6605f0bc2c609233751e27f3dbb38e42e3217
futratech <futura.vijayashaarathy@gmail.com>
2021-10-09 09:28:53 +0530

    add mailer in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'futuratechweb.herokuapp.com', protocol: 'https' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true 
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => "smnmalai@gmail.com",
+    :password             => "481012@Malai",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
