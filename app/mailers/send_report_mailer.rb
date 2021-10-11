594d39a211b0323329ff4948043999fb84d33919
Karthiga <karthigaanand14@gmail.com>
2021-10-11 16:38:34 +0530

    added delete file code

--- app/mailers/send_report_mailer.rb
+++ app/mailers/send_report_mailer.rb
@@ -3,7 +3,9 @@ class SendReportMailer < ApplicationMailer
 
   def send_report(invoice_id)
   	@invoice = Invoice.find invoice_id
-    attachments["premium_invoice_#{invoice_id}.pdf"] = File.read(Rails.root.join('tmp', "premium_invoice_#{@invoice.id}.pdf"))
-  	mail(to: @invoice.to_email, subject: 'Insurance Payment Online Receipt')  	
+    file_path = Rails.root.join('public', "premium_invoice_#{@invoice.id}.pdf")
+    attachments["premium_invoice_#{invoice_id}.pdf"] = File.read(file_path)
+  	mail(to: @invoice.to_email, subject: 'Insurance Payment Online Receipt')
+    File.delete(file_path) if File.exist?(file_path)
   end
 end
